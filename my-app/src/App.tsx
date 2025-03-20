581f7355d29edf6fff3995d7d408d83b291b3d60
Elizabeth <elizabeth.shiu@ibm.com>
2025-03-20 12:08:29 -0400

    pr api

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import { Button } from "@carbon/react";
 import { Octokit } from "@octokit/core";
@@ -7,28 +7,51 @@ const octokit = new Octokit({
   auth: process.env.REACT_APP_GH_TOKEN,
 });
 const ownerName = 'elizabethshiu';
-const repoName = 'https://github.com/elizabethshiu/validation-agent'
+const repoName = 'validation-agent'
+
+async function commitFile(contents: string) {
+  await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+    owner: ownerName,
+    repo: repoName,
+    path: 'PATH',
+    message: 'my commit message',
+    committer: {
+      name: 'Monalisa Octocat',
+      email: 'octocat@github.com'
+    },
+    content: 'bXkgbmV3IGZpbGUgY29udGVudHM=',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+}
+
+async function createPR(validationVersion: string) {
+  let date = new Date()
+  await octokit.request('POST /repos/{owner}/{repo}/pulls', {
+    owner: ownerName,
+    repo: repoName,
+    title: `${validationVersion}-${date.toLocaleString()}`,
+    body: `Validate configuration changes against OTel Collector ${validationVersion}`,
+    head: `validation`,
+    base: 'main',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  });
+}
+
 
 function App() {
   const [validationVersion, setValidationVersion] = useState('latest')
 
   function handleSetValidationVersion(event: { target: { value: React.SetStateAction<string>; }; }) {
     setValidationVersion(event.target.value);
+    
   }
 
   async function handleValidate() {
-    let date = new Date()
-    await octokit.request('POST /repos/{owner}/{repo}/pulls', {
-      owner: ownerName,
-      repo: repoName,
-      title: `${date.toLocaleString()}-${validationVersion}`,
-      body: `Validate configuration changes against OTel Collector ${validationVersion}`,
-      head: 'octocat:new-feature',
-      base: 'master',
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
-      }
-    });
+    
   }
 
   return (
